4a19de997635541a98e545e405f00e032f2e6b6a
bchartoff <bchartoff@gmail.com>
2016-03-14 17:54:35 -0400

    building image files

--- samurai.py
+++ samurai.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import re
+from colour import Color
+import json
 
 sourceDir = "source_videos"
 
@@ -48,4 +50,74 @@ def generateHist():
           # subprocess.call(['rm','histogram_image.txt'])
       # subprocess.call(['echo', 'foo\\n', '>>', 'output.json'], stdout=fp)
 
-generateHist()
+# generateHist()
+
+def cleanHist():
+  for file in os.listdir('data'):
+    if(file == '.DS_Store'):
+      continue
+    with open('data/%s'%file) as f:
+      lines = f.readlines()
+      # print lines
+      for i, s in enumerate(lines):
+        # line 
+        hexGroups = re.search('(#.*) ', lines[i])
+        hexColor = hexGroups.group(1)
+        c = Color(hexColor)
+        lines[i] = c.hsl
+    # season = fileParts.group(1)
+
+      # lines[0:] = [line + 1 for line in lines[0:]]
+      # for line in lines:
+    # return lines
+    colors = lines
+    colors.sort(key=lambda tup: tup[2])
+
+    chunk = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)]
+
+    colors = chunk(colors, 200)
+
+    for row in colors:
+      row.sort(key=lambda tup: tup[0])
+
+    for row in colors:
+      row[0:] = [Color(hsl=c).hex for c in row[0:]]
+      
+    # print colors
+    h = len(colors)
+    w = len(colors[0])
+    with open('text/%s'%file, 'w') as f:
+      # ImageMagick pixel enumeration: 640,480,255,srgb
+      f.write("# ImageMagick pixel enumeration: %i,%i,255,srgb\n"%(w,h))
+      for r in range(0, h):
+        for c in range(0, w):
+          # print "%i,%i"%(c,r)
+          try:
+            col = colors[r][c]
+            f.write("%i,%i: "%(c,r))
+            rgb = Color(col).rgb
+            new = (int(rgb[0]*255), int(rgb[1]*255), int(rgb[2]*255))
+            f.write("%s  "%str(new))
+            f.write(col)
+            f.write("  srgb%s"%str(new))
+            f.write("\n")
+          except IndexError:
+            # print
+            break
+    subprocess.call(["convert", "text/%s"%file, "img/%s"%file.replace(".txt",".png")])
+          # print colors[r][c]
+
+
+      # f.write("var colors=")
+
+      # json.dump(colors, f, ensure_ascii=False)
+
+    # break
+
+cleanHist()
+# colors = temp
+#   c = Color(color)
+
+
+
+
